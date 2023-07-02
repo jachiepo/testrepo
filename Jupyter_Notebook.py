40b86a9e3e38fd03c66e596589f221a3c1812330
jachiepo <118017383+jachiepo@users.noreply.github.com>
2023-07-02 19:15:30 +0000

    Add files via upload
    Hands-on Lab: Create your Jupyter Notebook

---
+++ Jupyter_Notebook.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# #Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1.Python
+# 
+# 2.R
+# 
+# 3.SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1.Pandas
+# 
+# 2.Numpy
+# 
+# 3.Matplotlib
+
+# |Data Science Tools|
+# |------------------|
+# |Jupyther Notebook|
+# |Apache zeppelin|
+# |R studio|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# his a simple arithmetic expression to mutiply then add integers
+
+# In[9]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+# In[13]:
+
+
+200/60
+
+
+# **Objectives:**
+
+# - Python
+# - R
+# - SQL
+# - Scala
+# - Java
+
+# ## Author Joël ACHIEPO
